3377601ba2d60d37676a38e47988e7cb9fcf6d6e
༺ Initial H ༻ <alsyundawy@users.noreply.github.com>
2024-04-22 17:11:59 +0700

    Update export-zones-pds.sh

--- export-zones-pds.sh
+++ export-zones-pds.sh
@@ -2,21 +2,22 @@
 
 # creates dump/export/backup of all DNS zones
 
+# Check if /home/powerdns/zones directory exists, if not, create it
 if [ ! -d /home/powerdns/zones ]; then
-  if [ ! -d /home/powerdns ]; then
-    mkdir /home/powerdns
-  fi
-  mkdir /home/powerdns/zones
+  mkdir -p /home/powerdns/zones
 fi
 
-zones=(`/usr/bin/pdnsutil list-all-zones`)
-today=`date +%Y%m%d`
+# Get list of all zones
+zones=( $(/usr/bin/pdnsutil list-all-zones) )
 
-for z in "${!zones[@]}"
+# Get today's date in YYYYMMDD format
+today=$(date +%Y%m%d)
+
+# Loop through each zone and export it
+for z in "${zones[@]}"
 do
-
-  /usr/bin/pdnsutil list-zone ${zones[$z]} > "/home/powerdns/zones/${zones[$z]}-$today.zone"
-
+  /usr/bin/pdnsutil list-zone "$z" > "/home/powerdns/zones/${z}-${today}.zone"
 done
 
+# Remove zone files older than 28 days
 find /home/powerdns/zones/ -type f -name '*.zone' -mtime +28 -exec rm {} \;
